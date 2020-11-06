9785b03014cf92b5c3cbbb323882a5f821fcc40f
rjfmachado <rjfmachado@users.noreply.github.com>
2020-11-06 16:58:27 +0000

    updates to ipconfiguration

--- variables.tf
+++ variables.tf
@@ -26,9 +26,6 @@ variable "storage_type" {
   default = "Premium_LRS"
 }
 
-variable "subnetid" {
-}
-
 variable "cloud-config" {
   type    = string
   default = "dns.tpl"
@@ -37,8 +34,9 @@ variable "cloud-config" {
 variable "ipconfiguration" {
   type = object
   default = {
-    name                          = "ipconfig-${var.name}"
-    subnet_id                     = var.subnetid
+    name                          = "ipconfig-name"
+    subnet_id                     = "subnetid"
     private_ip_address_allocation = "Dynamic"
+    #private_ip_address            = ""
   }
 }
